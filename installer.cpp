6556b1578029027c92b02cb2f09fa810f1ecd1d7
Feeling97 <Feeling97@users.noreply.github.com>
2015-04-13 20:21:59 +0200

    Création des fichiers de l'installer

---
+++ installer.cpp
@@ -0,0 +1,36 @@
+#include "installer.h"
+
+QGridLayout* nextStage(QString client, QString os, int etape)
+{
+    QGridLayout *layout = new QGridLayout();
+    if (etape == 0) { // Etape 1: Présentation
+        layout->addWidget(new QLabel("Ce programme va installer " + client + " pour " + os + " avec la configuration recommandée"), 0, 0);
+    }
+    return layout;
+}
+
+QString determineClient(QString os)
+{
+    if (os == "Windows 8.1")
+        return "qBittorrent";
+    else if (os == "Windows 8")
+        return "qBittorrent";
+    else if (os == "Windows 7")
+        return "µTorrent 2.2.1";
+    else if (os == "Windows Vista")
+        return "µTorrent 2.2.1";
+    else if (os == "Windows XP x64")
+        return "µTorrent 2.2.1";
+    else if (os == "Windows XP")
+        return "µTorrent 2.2.1";
+    else if (os == "Windows 2000")
+        return "µTorrent 2.2.1";
+    else if (os == "Windows NT")
+        return "µTorrent 2.2.1";
+    else
+    {
+        QMessageBox::critical(0, "Erreur", "Impossible de déterminer le client correspondant à votre système<br />Vous pouvez utiliser le checker en ligne :<br /><a href='http://irc.t411.io/checker/'>Cliquez ici</a>");
+        // A faire -- Empêcher l'utilisateur d'accéder au programme
+        return "";
+    }
+}
---
+++ installer.h
@@ -0,0 +1,13 @@
+#ifndef INSTALLER_H
+#define INSTALLER_H
+
+#include <QString>
+#include <QGridLayout>
+#include <QLabel>
+#include <QMessageBox>
+
+QGridLayout* nextStage(QString client, QString os, int etape);
+QString determineClient(QString os);
+
+#endif // INSTALLER
+
