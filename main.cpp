8577650a98311aae0d45703d484f67efb8be5c83
Feeling97 <Feeling97@users.noreply.github.com>
2015-04-13 19:33:25 +0200

    Mise en place de l'UTF-8

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    #if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+        QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+    #else
+        QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    #endif
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
